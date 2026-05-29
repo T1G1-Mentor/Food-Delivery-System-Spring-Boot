d715215a1c7b97dcb4e575fe9a8e0f5e3814dabd
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2026-05-29 17:55:42 +0300

    new: CustomerAddressController     - seperating it from our CustomerController

---
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/customer/controller/CustomerAddressController.java
@@ -0,0 +1,62 @@
+package com.mentorship.food_delivery_app.customer.controller;
+
+import com.mentorship.food_delivery_app.customer.dto.customeraddress.request.CustomerAddressRequestDto;
+import com.mentorship.food_delivery_app.customer.dto.customeraddress.request.ModifyCustomerAddressRequestDto;
+import com.mentorship.food_delivery_app.customer.dto.customeraddress.response.CustomerAddressResponseDto;
+import com.mentorship.food_delivery_app.customer.entity.Customer;
+import com.mentorship.food_delivery_app.customer.service.contract.CustomerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/customers/addresses")
+@RequiredArgsConstructor
+public class CustomerAddressController {
+    private final CustomerService customerService;
+
+    @GetMapping
+    public ResponseEntity<List<CustomerAddressResponseDto>> getAllAddresses() {
+        Customer customer = customerService.getLoggedinCustomer(); // will be replaced with authorization principal to get the id
+
+        return ResponseEntity.ok(customerService.getAllCustomerAddresses(customer.getId()));
+    }
+
+    @GetMapping("/{addressId}")
+    public ResponseEntity<CustomerAddressResponseDto> getAddress(@PathVariable UUID addressId) {
+        Customer customer = customerService.getLoggedinCustomer(); // will be replaced with authorization principal to get the id
+
+        return ResponseEntity.ok(customerService.getCustomerAddress(addressId,
+                customer.getId()));
+    }
+
+    @DeleteMapping("/{addressId}")
+    public ResponseEntity<Void> deleteCustomerAddress(@PathVariable UUID addressId) {
+        customerService.deleteCustomerAddress(addressId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> createCustomerAddress(@RequestBody @Valid CustomerAddressRequestDto addressRequestDto) {
+        UUID addressId = customerService.createCustomerAddress(addressRequestDto);
+
+        return ResponseEntity.
+                created(URI.create("/api/v1/customers/addresses/" + addressId)).build();
+    }
+
+    @PutMapping("/{addressId}")
+    public ResponseEntity<Void> updateCustomerAddress(@RequestBody @Valid ModifyCustomerAddressRequestDto addressRequestDto,
+                                                      @PathVariable UUID addressId) {
+        Customer customer = customerService.getLoggedinCustomer(); // will be replaced with authorization principal to get the id
+
+        customerService.updateCustomerAddress(addressId, customer.getId(), addressRequestDto);
+
+        return ResponseEntity.noContent().build();
+    }
+}
