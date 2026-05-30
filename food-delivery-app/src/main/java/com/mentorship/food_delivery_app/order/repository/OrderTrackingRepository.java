154c92bb6c32b882803c72023eb5db59d58a55e8
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2026-05-30 19:40:52 +0300

    new: OrderTrackingService     - Retreving the order tracking history DTO directly from the DB     - ordered by creation time DESC

--- food-delivery-app/src/main/java/com/mentorship/food_delivery_app/order/repository/OrderTrackingRepository.java
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/order/repository/OrderTrackingRepository.java
@@ -1,9 +1,24 @@
 package com.mentorship.food_delivery_app.order.repository;
 
+import com.mentorship.food_delivery_app.order.dto.response.OrderTrackingDto;
 import com.mentorship.food_delivery_app.order.entity.OrderTracking;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OrderTrackingRepository extends CrudRepository<OrderTracking, UUID> {
+    @Query("""
+    SELECT new com.mentorship.food_delivery_app.order.dto.response.OrderTrackingDto(
+    od.status,
+    od.description,
+    od.createdAt
+    )
+    FROM OrderTracking od
+    WHERE od.order.id = :orderId AND od.order.customer.id = :customerId
+    ORDER BY od.createdAt DESC
+""")
+    List<OrderTrackingDto> findAllByCustomerIdAndOrderId(UUID customerId, UUID orderId);
+
 }
---
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/order/service/implementation/OrderTrackingServiceImp.java
@@ -0,0 +1,23 @@
+package com.mentorship.food_delivery_app.order.service.implementation;
+
+import com.mentorship.food_delivery_app.order.dto.response.OrderTrackingDto;
+import com.mentorship.food_delivery_app.order.repository.OrderTrackingRepository;
+import com.mentorship.food_delivery_app.order.service.contract.OrderTrackingService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderTrackingServiceImp implements OrderTrackingService {
+    private final OrderTrackingRepository orderTrackingRepository;
+
+    @Override
+    public List<OrderTrackingDto> getTrackingHistory(UUID customerId, UUID orderId) {
+        return orderTrackingRepository.findAllByCustomerIdAndOrderId(customerId, orderId);
+    }
+}
