1417f7bc19d1070f36d95b29a3e1945b28195cde
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2026-06-10 04:00:16 +0300

    restaurant domain new queries for fetching restaurant branch and the refactor in the coupon

--- food-delivery-app/src/main/java/com/mentorship/food_delivery_app/restaurant/repository/CouponRepository.java
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/restaurant/repository/CouponRepository.java
@@ -1,12 +1,16 @@
 package com.mentorship.food_delivery_app.restaurant.repository;
 
 import com.mentorship.food_delivery_app.restaurant.entity.Coupon;
-import com.mentorship.food_delivery_app.restaurant.entity.Restaurant;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CouponRepository extends CrudRepository<Coupon, UUID> {
-    Optional<Coupon> findByCouponIdAndRestaurant(UUID couponId, Restaurant restaurant);
+    @Query("""
+    SELECT c FROM Coupon c
+    WHERE c.couponId = :couponId
+""")
+    Optional<Coupon> findByCouponId(UUID couponId);
 }
--- food-delivery-app/src/main/java/com/mentorship/food_delivery_app/restaurant/repository/RestaurantBranchRepository.java
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/restaurant/repository/RestaurantBranchRepository.java
@@ -1,9 +1,16 @@
 package com.mentorship.food_delivery_app.restaurant.repository;
 
 import com.mentorship.food_delivery_app.restaurant.entity.RestaurantBranch;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RestaurantBranchRepository extends CrudRepository<RestaurantBranch, UUID> {
+    @Query("""
+    SELECT r FROM RestaurantBranch r
+    WHERE r.restaurantBranchId = :restaurantBranchId
+""")
+    Optional<RestaurantBranch> findByRestaurantBranchId(UUID restaurantBranchId);
 }
