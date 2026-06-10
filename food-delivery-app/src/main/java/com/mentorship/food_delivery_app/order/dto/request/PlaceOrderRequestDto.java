4f5929466ddeaa0acc13ed17198ceeab292dfcab
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2026-06-10 04:16:01 +0300

    refactor:     - DeliveryAddressDto         - adding not null constraint         - new from message to convert from the entity to the DTO     - PlaceOrderRequestDto         - specifieng more request data.     - OrderResponseDto         - Changing the fields naming         - adding more entries from more verpose response

--- food-delivery-app/src/main/java/com/mentorship/food_delivery_app/order/dto/request/PlaceOrderRequestDto.java
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/order/dto/request/PlaceOrderRequestDto.java
@@ -1,12 +1,24 @@
 package com.mentorship.food_delivery_app.order.dto.request;
 
+import com.mentorship.food_delivery_app.cart.dto.request.CartItemRequestDto;
+import com.mentorship.food_delivery_app.payment.entity.enums.PaymentMethod;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.List;
 import java.util.UUID;
 
 public record PlaceOrderRequestDto(
         @NotNull
         DeliveryAddressDto deliveryAddress,
-        UUID couponId
+        UUID couponId,
+        @NotNull
+        UUID restaurantBranchId,
+        @NotNull
+        List<CartItemRequestDto> cartItems,
+        @NotNull
+        PaymentMethod paymentMethod,
+        @NotNull
+        UUID cartId,
+        String orderNotes
 ) {
 }
--- food-delivery-app/src/main/java/com/mentorship/food_delivery_app/order/dto/response/OrderResponseDto.java
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/order/dto/response/OrderResponseDto.java
@@ -1,15 +1,22 @@
 package com.mentorship.food_delivery_app.order.dto.response;
 
+import com.mentorship.food_delivery_app.order.dto.request.DeliveryAddressDto;
+import lombok.Builder;
+
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
+@Builder
 public record OrderResponseDto(
-        UUID id,
-        BigDecimal subTotal,
-        BigDecimal fee,
-        BigDecimal discount,
-        BigDecimal total,
+        UUID orderId,
+        UUID restaurantBranchId,
+        String restaurantName,
+        BigDecimal orderSubtotal,
+        BigDecimal orderFee,
+        BigDecimal orderDiscount,
+        BigDecimal orderTotal,
+        DeliveryAddressDto orderDeliveryAddress,
         List<OrderItemResponseDto> orderItems
 ) {
 
